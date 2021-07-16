9f6335825b074e7d0e52df71e42b5b3806ba4677
Pooja-boot-git <srivastava_pooja@hotmail.com>
2021-07-16 01:05:27 -0400

    added polyline

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,26 +1,33 @@
 // Get data from cities.js
 let cityData = cities;
+// center of the map set to SFO
+var map = L.map('mapid',{
+    center: [37.6213, -122.3790],
+    zoom: 5});
 
-var map = L.map('mapid').setView([40.7, -94.5], 5);
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: ((city.population)/200000),
-    lineweight: 6,
-    color: "orange",
-    fillColor: "yellow"
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+//Los Angeles International Airport (LAX) TO San Francisco International Airport (SFO),
+//Intermediate point: Salt Lake City International Airport (SLC) 
+// ending point: Seattle-Tacoma ,
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: "4 4",
+  dashOffset: "20"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
